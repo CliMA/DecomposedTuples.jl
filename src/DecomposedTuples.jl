40ec884aaba31db1605eb35bd3319854f069762a
Charles Kawczynski <kawczynski.charles@gmail.com>
2024-03-27 14:38:51 -0400

    Add docs

--- src/DecomposedTuples.jl
+++ src/DecomposedTuples.jl
@@ -57,7 +57,15 @@ which can be indexed in a similar way to the original
 tuple:
 
 ```julia
-
+import DecomposedTuples as DT
+tup = (Foo1(), Foo2(), Foo3(), Foo4(), Foo3(), Foo3())
+dtup = DT.DecomposedTuple(tup)
+@test dtup.sparse_ntuples[1][1] === tup[1]
+@test dtup.sparse_ntuples[2][2] === tup[2]
+@test dtup.sparse_ntuples[3][3] === tup[3]
+@test dtup.sparse_ntuples[4][4] === tup[4]
+@test dtup.sparse_ntuples[3][5] === tup[5]
+@test dtup.sparse_ntuples[3][6] === tup[6]
 ```
 """
 struct DecomposedTuple{SC}
